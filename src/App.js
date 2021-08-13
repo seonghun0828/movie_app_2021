10a8cd0e2a8e2beea12b20b7602adf60aca0bc4c
seonghun <seonghunkim468@gmail.com>
2021-08-13 17:29:50 +0900

    React practice

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import axios from 'axios';
 import Movie from './Movie';
 import './App.css';
+import { Fragment } from 'react';
+
+// api 받아와서 Movie 인자로 보내주고 mount async
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
+
   getMovies = async () => {
-    // const aa = await axios.get('https://yts-proxy.now.sh/list_movies.json');
-    // console.log(aa.data.data.movies);
     const {
       data: {
         data: { movies },
@@ -18,37 +20,88 @@ class App extends React.Component {
     } = await axios.get(
       'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
     );
-    this.setState({ movies, isLoading: false });
-    // this.setState({movies: movies}); 와 같음. 앞은 from setState, 뒤는 from axios
+    this.setState({
+      movies,
+      isLoading: false,
+    });
   };
+
   componentDidMount() {
     this.getMovies();
   }
+
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <section className="container">
+      <Fragment>
         {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
+          <div className="loader">Loading by rating...</div>
         ) : (
           <div className="movies">
             {movies.map((movie) => (
               <Movie
                 key={movie.id}
                 id={movie.id}
-                year={movie.year}
                 title={movie.title}
+                year={movie.year}
+                genres={movie.genres}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
-                genres={movie.genres}
               />
             ))}
           </div>
         )}
-      </section>
+      </Fragment>
     );
   }
 }
+
+// class App extends React.Component {
+//   state = {
+//     isLoading: true,
+//     movies: [],
+//   };
+//   getMovies = async () => {
+// const aa = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+// console.log(aa.data.data.movies);
+//     const {
+//       data: {
+//         data: { movies },
+//       },
+//     } = await axios.get(
+//       'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
+//     );
+//     this.setState({ movies, isLoading: false });
+//     // this.setState({movies: movies}); 와 같음. 앞은 from setState, 뒤는 from axios
+//   };
+//   componentDidMount() {
+//     this.getMovies();
+//   }
+//   render() {
+//     const { isLoading, movies } = this.state;
+//     return (
+//       <section className="container">
+//         {isLoading ? (
+//           <div className="loader">
+//             <span className="loader__text">Loading...</span>
+//           </div>
+//         ) : (
+//           <div className="movies">
+//             {movies.map((movie) => (
+//               <Movie
+//                 key={movie.id}
+//                 id={movie.id}
+//                 year={movie.year}
+//                 title={movie.title}
+//                 summary={movie.summary}
+//                 poster={movie.medium_cover_image}
+//                 genres={movie.genres}
+//               />
+//             ))}
+//           </div>
+//         )}
+//       </section>
+//     );
+//   }
+// }
 export default App;
--- src/Movie.js
+++ src/Movie.js
@@ -1,35 +1,66 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Fragment } from 'react';
 import './Movie.css';
 // state 가 필요 없으므로 class일 필요가 없음. function component로
 
-function Movie({ year, title, summary, poster, genres }) {
+function Movie(args) {
+  const { title, year, genres, summary, poster } = args;
   return (
-    <div className="movie">
+    <Fragment>
       <img src={poster} title={title} alt={title} />
-      <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
+      <div className="movie_data">
+        <h3 className="movie_title">{title}</h3>
+        <h5 className="movie_year">{year}</h5>
         <ul className="genres">
           {genres.map((genre, index) => (
-            <li key={index} className="genres__genre">
+            <li key={index} className="genres_genre">
               {genre}
             </li>
           ))}
         </ul>
         <p className="movie_summary">{summary}</p>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
 Movie.propTypes = {
   id: PropTypes.number.isRequired,
-  year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
+
+// function Movie({ year, title, summary, poster, genres }) {
+//   return (
+//     <div className="movie">
+//       <img src={poster} title={title} alt={title} />
+//       <div className="movie__data">
+//         <h3 className="movie__title">{title}</h3>
+//         <h5 className="movie__year">{year}</h5>
+//         <ul className="genres">
+//           {genres.map((genre, index) => (
+//             <li key={index} className="genres__genre">
+//               {genre}
+//             </li>
+//           ))}
+//         </ul>
+//         <p className="movie_summary">{summary}</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// Movie.propTypes = {
+//   id: PropTypes.number.isRequired,
+//   year: PropTypes.number.isRequired,
+//   title: PropTypes.string.isRequired,
+//   summary: PropTypes.string.isRequired,
+//   poster: PropTypes.string.isRequired,
+//   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+// };
 
 export default Movie;
